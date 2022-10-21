a1625ea01aefea94ea675269edb1192808d590c0
xdefidev <xdefidev@gmail.com>
2022-10-21 17:27:43 +0100

    change

---
+++ pages/portfolio.js
@@ -0,0 +1,27 @@
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>0xDefiDev Portfolio</title>
+
+        <meta property="og:title" content="Xdefidev Dev Portfolio" />
+
+        <meta
+          property="og:description"
+          content="Need Blockchain Developer? View our Portfolio"
+        />
+
+        <meta property="og:url" content="https://xdefidev.shop" />
+
+        <meta
+          property="og:image"
+          content="https://ik.imagekit.io/lzgpc48la/Yellow_Bright_Business_Idea_Tutorial_Youtube_Thumbnail_n186Tgza0.png?ik-sdk-version=javascript-1.4.3&updatedAt=1665836822605"
+        />
+      </Head>
+      <Navbar />
+      <div></div>
+    </div>
+  );
+}
