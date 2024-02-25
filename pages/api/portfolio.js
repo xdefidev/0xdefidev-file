c072b03e3caa118dc0f630baaf09c62bb2f4792a
xdefidev <xdefidev@gmail.com>
2024-02-25 20:46:14 +0100

    ch

--- pages/api/portfolio.js
+++ pages/api/portfolio.js
@@ -1,20 +1,5 @@
 export default [
-  {
-    name: "Pancakeswap Clone",
-    link: "https://stately-empanada-b6c255.netlify.app/",
-    image:
-      "https://ik.imagekit.io/lzgpc48la/scrnli_02_01_2023_12-07-03_u9QCCX70z.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672658107714",
-    description:
-      "Decentralised excange | Pancakeswap clone with swap, farms, staking pools, lottery and Prediction games",
-  },
-  {
-    name: "AutoStaking Dapp",
-    link: "https://xdefidev.github.io/metafit-dapp/",
-    image:
-      "https://ik.imagekit.io/lzgpc48la/scrnli_02_01_2023_12-05-05_dR_gGf4Em.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672658103544",
-    description:
-      "Autostaking Dapp with APY calculator and token smart contract with auto rebase",
-  },
+ 
   {
     name: "Multi Plan Staking Dapp",
     link: "https://rococo-travesseiro-ee2ae5.netlify.app/",
@@ -22,6 +7,14 @@ export default [
       "https://ik.imagekit.io/lzgpc48la/scrnli_02_03_2023_17-22-28_9waSDeM9v.png?ik-sdk-version=javascript-1.4.3&updatedAt=1677774182071",
     description:
       "Manual Staking Dapp with multiple plans (more than one APY and timeframe). Developed with React, Solidity and web3.",
+  },
+  {
+    name: "Pinksale Lock Clone",
+    link: "https://startling-stardust-9d585d.netlify.app/Create",
+    image:
+      "https://ik.imagekit.io/hp2oyifpf/Screenshot%202024-02-25%20at%2020.38.27.png?updatedAt=1708890304246",
+    description:
+      "Exact Pinksale Lock with Token and LP lock.",
   },
   {
     name: "Presale DApp",
@@ -66,5 +59,21 @@ export default [
       "https://ik.imagekit.io/hp2oyifpf/scrnli_28_11_2023_12-33-34.png?updatedAt=1701171238689",
     description:
       "NFT marketplace with AI generator, Fast and seamless service.",
+  },
+  {
+    name: "Pancakeswap Clone",
+    link: "https://stately-empanada-b6c255.netlify.app/",
+    image:
+      "https://ik.imagekit.io/lzgpc48la/scrnli_02_01_2023_12-07-03_u9QCCX70z.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672658107714",
+    description:
+      "Decentralised excange | Pancakeswap clone with swap, farms, staking pools, lottery and Prediction games",
+  },
+  {
+    name: "AutoStaking Dapp",
+    link: "https://xdefidev.github.io/metafit-dapp/",
+    image:
+      "https://ik.imagekit.io/lzgpc48la/scrnli_02_01_2023_12-05-05_dR_gGf4Em.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672658103544",
+    description:
+      "Autostaking Dapp with APY calculator and token smart contract with auto rebase",
   },
 ];
