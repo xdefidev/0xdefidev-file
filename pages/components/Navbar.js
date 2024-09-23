77096bda89087e41087032540e660fe46e79326e
xdefidev <xdefidev@gmail.com>
2024-09-23 09:43:04 +0100

    ch

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -19,8 +19,18 @@ export default function Navbar() {
           </a>
         </li>
         <li>
-          <a href="/services" className="font-josefin font-semibold">
-            Services
+          <a href="/services" className="font-josefin font-semibold flex">
+            Services{" "}
+            <span>
+              <svg
+                width="11"
+                height="7"
+                viewBox="0 0 11 7"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M5.4636 6.38899L0.839326 1.769C0.692474 1.62109 0.692474 1.38191 0.839326 1.23399L1.45798 0.61086C1.60483 0.462945 1.84229 0.462945 1.98915 0.61086L5.72919 4.34021L9.46923 0.61086C9.61608 0.462945 9.85354 0.462945 10.0004 0.61086L10.619 1.23399C10.7659 1.38191 10.7659 1.62109 10.619 1.769L5.99477 6.38899C5.84792 6.5369 5.61046 6.5369 5.4636 6.38899Z"></path>
+              </svg>
+            </span>
           </a>
         </li>
         <li>
@@ -28,7 +38,7 @@ export default function Navbar() {
             Portfolio
           </a>
         </li>
-        
+
         <li>
           <a
             href="https://t.me/xdefideveloper"
@@ -71,7 +81,7 @@ export default function Navbar() {
                 Home
               </li>
             </a>
-           
+
             <a href="/services" target="_blank" className="w-full">
               <li
                 className={`mx-4 text-lg cursor-pointer font-josefin font-semibold hover:text-[#0462f7]`}
@@ -86,7 +96,11 @@ export default function Navbar() {
                 Portfolio
               </li>
             </a>
-            <a href="https://t.me/xdefideveloper" target="_blank" className="w-full">
+            <a
+              href="https://t.me/xdefideveloper"
+              target="_blank"
+              className="w-full"
+            >
               <li
                 className={`mx-4 text-lg cursor-pointer font-josefin font-semibold hover:text-[#0462f7]`}
               >
