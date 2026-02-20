cb23812fa813835f247aa8b19982a5b1e021cc0e
“isla13coin” <isla13coin@gmail.com>
2026-02-20 21:47:43 +0100

    ch

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,83 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link
+        <link
           href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap"
           rel="stylesheet"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@graph": [
+                {
+                  "@type": "WebSite",
+                  "@id": "https://0xdefidev.com/#website",
+                  "url": "https://0xdefidev.com/",
+                  "name": "Xdefidev",
+                  "description": "Premium Blockchain, Crypto Exchange, and Web3 Development Services",
+                  "potentialAction": [
+                    {
+                      "@type": "SearchAction",
+                      "target": {
+                        "@type": "EntryPoint",
+                        "urlTemplate": "https://0xdefidev.com/search?q={search_term_string}"
+                      },
+                      "query-input": "required name=search_term_string"
+                    }
+                  ]
+                },
+                {
+                  "@type": "SiteNavigationElement",
+                  "@id": "https://0xdefidev.com/#navigation",
+                  "name": "Main Navigation",
+                  "hasPart": [
+                    {
+                      "@type": "WebPage",
+                      "name": "Blockchain Development",
+                      "url": "https://0xdefidev.com/categories/blockchain-development"
+                    },
+                    {
+                      "@type": "WebPage",
+                      "name": "Crypto Exchange Development",
+                      "url": "https://0xdefidev.com/categories/crypto-exchange-development"
+                    },
+                    {
+                      "@type": "WebPage",
+                      "name": "Telegram Bots and Mini Apps",
+                      "url": "https://0xdefidev.com/categories/telegram-bots"
+                    },
+                    {
+                      "@type": "WebPage",
+                      "name": "Crypto Casino and Game Development",
+                      "url": "https://0xdefidev.com/categories/crypto-casino-development"
+                    },
+                    {
+                      "@type": "WebPage",
+                      "name": "Defi & NFT Ecosystems",
+                      "url": "https://0xdefidev.com/categories/defi-nft-ecosystems"
+                    },
+                    {
+                      "@type": "WebPage",
+                      "name": "Portfolio",
+                      "url": "https://0xdefidev.com/portfolio"
+                    },
+                    {
+                      "@type": "WebPage",
+                      "name": "Our Services",
+                      "url": "https://0xdefidev.com/services"
+                    },
+                    {
+                      "@type": "WebPage",
+                      "name": "Contact Us",
+                      "url": "https://0xdefidev.com/contact-us"
+                    }
+                  ]
+                }
+              ]
+            })
+          }}
         />
       </Head>
       <body>
